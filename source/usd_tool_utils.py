0a7628d43a592041a2ff851ca4d15a45f6002ced
alesiakarch <karchevskaya.alesya@gmail.com>
2025-05-07 00:45:38 +0100

    testing the create layer

--- source/usd_tool_utils.py
+++ source/usd_tool_utils.py
@@ -5,7 +5,7 @@ def get_stage(usd_dir, stage_name):
     """
     Get the USD stage from the USD project root folder.
     """
-    stage_path = f"{usd_dir}{stage_name}"
+    stage_path = f"{usd_dir}/{stage_name}"
     print(f"Loading USD stage from: {stage_path}")
     stage = Usd.Stage.Open(stage_path)
     return stage
@@ -14,4 +14,11 @@ def create_layer(usd_dir, layer_name):
     """
     Create a new USD layer and add it to the root layer.
     """
-    ...
+    print("Creating new USD layer...")
+    layer_path = f"{usd_dir}/{layer_name}"
+    usd_layer = Sdf.Layer.CreateNew(layer_path)
+    usd_layer.comment = "Created new usd layer"
+
+    # stage.GetRootLayer().subLayerPaths.append(rig_layer_path)
+    # print (stage)
+    # stage.GetRootLayer().Save()
--- tests/test_usd_tool_utils.py
+++ tests/test_usd_tool_utils.py
@@ -3,30 +3,49 @@ import os
 import tempfile
 import maya.standalone
 import maya.cmds as cmds
-from source.usd_tool_utils import get_stage
+from pxr import Sdf, Usd, UsdGeom
+from source.usd_tool_utils import get_stage, create_layer
+
+@pytest.fixture
+def maya_setup():
+    """
+    Fixture to set up and tear down the Maya standalone environment.
+    """
+    maya.standalone.initialize()
+    yield
+    if "mayaUsdPlugin" in maya.cmds.pluginInfo(query=True, listPlugins=True):
+        maya.cmds.unloadPlugin("mayaUsdPlugin", force=True)
+    maya.standalone.uninitialize()
 
 def test_get_stage():
     """
     Test the get_stage function returns a valid USD stage.
     """
-    maya.standalone.initialize()
-    try:
-        cmds.loadPlugin("mayaUsdPlugin")
-        #cmds.loadPlugin("vrayformaya")
-        from pxr import Sdf, Usd, UsdGeom
-        # Create temporarty directory and file for testing
-        with tempfile.TemporaryDirectory() as temp_dir:
-            test_stage_name = "test_stage.usda"
-            stage_path = os.path.join(temp_dir, test_stage_name)
+    # Create temporarty directory and file for testing
+    with tempfile.TemporaryDirectory() as temp_dir:
+        test_stage_name = "test_stage.usda"
+        stage_path = os.path.join(temp_dir, test_stage_name)
 
-            # Create a temporary USD stage file
-            usd_file = Sdf.Layer.CreateNew(stage_path)
-            assert usd_file != None, "Failed to create temporary USD stage file"
-            
-            # Test the get_stage function
-            stage = get_stage(temp_dir, test_stage_name)
-            #assert stage != None, "Failed to get USD stage"
-            #assert isinstance(stage, Usd.Stage), "Returned object is not a USD stage"
-            #assert stage.GetRootLayer().identifier == stage_path, "Stage path does not match expected path"
-    finally:    
-        maya.standalone.uninitialize()
+        # Create a temporary USD stage file
+        usd_file = Sdf.Layer.CreateNew(stage_path)
+        assert usd_file != None, "Failed to create temporary USD stage file"
+        
+        # Test the get_stage function
+        stage = get_stage(temp_dir, test_stage_name)
+        assert stage != None, "Failed to get USD stage"
+        assert isinstance(stage, Usd.Stage), "Returned object is not a USD stage"
+        assert stage.GetRootLayer().identifier == stage_path, "Stage path does not match expected path"
+
+
+def test_create_layer():
+    """
+    Test the create_layer function creates a new USD layer.
+    """
+    with tempfile.TemporaryDirectory() as temp_dir:
+        layer_name = "test_layer.usda"
+        layer_path = os.path.join(temp_dir, layer_name)
+
+        # Test the create_layer function
+        new_layer = create_layer(temp_dir, layer_name)
+        assert new_layer != None, "Failed to create a new USD layer"
+
