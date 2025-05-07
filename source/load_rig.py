32f8801618eb2e26b967934cc493daf7ca8dee3f
alesiakarch <karchevskaya.alesya@gmail.com>
2025-05-07 20:08:38 +0100

    abstracted the load rig function, ready to test

--- source/load_rig.py
+++ source/load_rig.py
@@ -3,73 +3,107 @@
 # First button: to Load the rig 
 # To load the rig the user must already have a USD stage file with the layout of the scene they are working from
 # the rig can be loaded a) into the LAY layer, the animated into the ANIM layer, or b) into the ANIM layer only
-# or c) into a RIG layer which purpise is solely to hold rig date and pass it on to animation
+# or c) into a RIG layer which purpose is solely to hold rig date and pass it on to animation
 # I'll hardcode the "rigs" Xform for now to focus on the loading part
 # I think in production the proxy rigs are added to layout to convey which characters are in the scene, and then the animators load the rig into the ANIM layer
 import os
 import maya.cmds as cmds
 from pxr import Usd, Sdf, UsdGeom
 import mayaUsdAddMayaReference as mr
+import usd_tool_utils as usd_utils
 
-def load_rig():
+
+project_root = "/home/s5221034/pipeline-project-alesiakarch/maya_test_project/"
+rig_layer_name = "Test_Scene_RIG.usda"
+asset_path = f"{project_root}assets/Wall-e_Rigs/Wall-e_RIG_v001.mb"
+
+def load_rig(usd_dir, stage_name):
     """
     Load the rig into the USD stage.
     """
     # get the USD stage
-    stage = get_stage()
+    stage = usd_utils.get_stage(project_root, stage_name)
     # create the RIG layer
-    create_layer()
+    rig_layer = usd_utils.create_layer(stage, project_root, rig_layer_name)
     # create maya reference
-    create_maya_ref()
-
-# get and initialise the USD stage
-def get_stage():
-    ...
-    # project_root = "/home/s5221034/pipeline-project-alesiakarch/maya_test_project/"
-    # stage = Usd.Stage.Open(f"{project_root}TestScene_stage.usda")
-    # print(project_root)
-
-# create RIG layer
-# add the Rig layer to the root layer and save
-def create_layer():
-    ...
-    # rig_layer_path = "/home/s5221034/pipeline-project-alesiakarch/maya_test_project/Test_Scene_RIG.usda"
-    # rig_layer = Sdf.Layer.CreateNew(rig_layer_path)
-    # rig_layer.comment = "RIG layer containing character rig references"
-
-    # stage.GetRootLayer().subLayerPaths.append(rig_layer_path)
-    # print (stage)
-    # stage.GetRootLayer().Save()
-
-def create_maya_ref():
-    ... 
-    # ask user for the asset path and set active layer to RIG so this is where we are writing
-    rig_path = f"{project_root}assets/Wall-e_Rigs/Wall-e_RIG_v001.mb"
+    #create_maya_ref(stage, )
     stage.SetEditTarget(rig_layer)
 
-    # create rig Xform
-    prim = stage.DefinePrim("/rigs", "Xform")
-    #loaded_rig = stage.DefinePrim("/rigs/Walee_001", "MayaReference")
+    # check if the rigs xform exist
+    rigs_grp = stage.GetPrimAtPath("/rigs")
+    if rigs_grp.IsValid():
+        print("/rigs group already exists")
+    else:
+        stage.DefinePrim("/rigs", "Xform")
+        rigs_grp = stage.GetPrimAtPath("/rigs")
+        print("created the /rigs group")
+    
+    # Defining the usd rig paths and namespace
+    # detect the stage
+    proxy_shapes = cmds.ls(type = "mayaUsdProxyShape") # lists in reverse order
+    print(proxy_shapes)
+    if not proxy_shapes:
+        raise RuntimeError("No USD stage found in the Maya scene!")
+    if len(proxy_shapes)>1:
+        raise RuntimeError("More than one USD stage detected. Please keep to one stage per scene.")
+    
+    usd_proxy_shape = proxy_shapes[0] # use the first proxy shape
+    parent_path = cmds.listRelatives(usd_proxy_shape, allParents=True, fullPath = True, shapes = True)
+    dag_path = f"{parent_path[0]}|{usd_proxy_shape}"
 
-    # Defining the paths and namespace
-    ufe_path = "|TestScene_stage|TestScene_stageShape,/rigs"  # Path to the rig's location in the USD stage (copies from MEL why is there a comma?)
-    maya_ref_path = f"{project_root}/assets/Wall-e_Rigs/Wall-e_RIG_v001.mb"  # Path to the Maya reference
+    rig_path = str(rigs_grp.GetPath())
+    path_to_usd_rig = f"{dag_path},{rig_path}"
+    print(path_to_usd_rig) 
+
+    # Path to the rig's location in the USD stage
     maya_namespace = "Wall_e_RIG_v002"  # Namespace for the Maya reference
 
     # load the rig into the Xform created above ("add maya reference") dont forget the namespace
     mr.createMayaReferencePrim(
-        ufePathStr=ufe_path,
-        mayaReferencePath=maya_ref_path,
-        mayaReferencePrimName = "MayaReference2", # this will be namespace too
+        ufePathStr=path_to_usd_rig,
+        mayaReferencePath=asset_path,
+        mayaReferencePrimName = maya_namespace, # this will be namespace too
         mayaNamespace=maya_namespace,
         variantSet = ("Walle_maya", "Walle_USD_Cache")
     )
 
     # edit as maya data 
-    cmds.mayaUsdEditAsMaya("|TestScene_stage|TestScene_stageShape,/rigs/MayaReference2")
+    cmds.mayaUsdEditAsMaya(f"{path_to_usd_rig}/{maya_namespace}")
 
     # save RIG layer
     stage.GetRootLayer().Save()
 
-# this shouold probably have a lot of tests and checks that everything is in the same place, but this is skeleton code
+
+
+def create_maya_ref(stage, asset_path, rig_layer):
+    ...
+    # ask user for the asset path and set active layer to RIG so this is where we are writing
+    # asset_path = f"{project_root}assets/Wall-e_Rigs/Wall-e_RIG_v001.mb"
+    # stage.SetEditTarget(rig_layer)
+
+    # create rig Xform (check for it first)
+    #prim = stage.DefinePrim("/rigs", "Xform")
+    #loaded_rig = stage.DefinePrim("/rigs/Walee_001", "MayaReference")
+
+    # Defining the paths and namespace
+    # ufe_path = "|TestScene_stage|TestScene_stageShape,/rigs"  # Path to the rig's location in the USD stage (copies from MEL why is there a comma?)
+    # maya_ref_path = f"{project_root}/assets/Wall-e_Rigs/Wall-e_RIG_v001.mb"  # Path to the Maya reference
+    # maya_namespace = "Wall_e_RIG_v002"  # Namespace for the Maya reference
+
+    # # load the rig into the Xform created above ("add maya reference") dont forget the namespace
+    # mr.createMayaReferencePrim(
+    #     ufePathStr=ufe_path,
+    #     mayaReferencePath=maya_ref_path,
+    #     mayaReferencePrimName = "MayaReference2", # this will be namespace too
+    #     mayaNamespace=maya_namespace,
+    #     variantSet = ("Walle_maya", "Walle_USD_Cache")
+    # )
+
+    # # edit as maya data 
+    # cmds.mayaUsdEditAsMaya("|TestScene_stage|TestScene_stageShape,/rigs/MayaReference2")
+
+    # # save RIG layer
+    # stage.GetRootLayer().Save()
+
+# this should probably have a lot of tests and checks that everything is in the same place, but this is skeleton code
 # also abstract this into functions
--- tests/test_usd_tool_utils.py
+++ tests/test_usd_tool_utils.py
@@ -17,24 +17,24 @@ def maya_setup():
         maya.cmds.unloadPlugin("mayaUsdPlugin", force=True)
     maya.standalone.uninitialize()
 
-# def test_get_stage():
-#     """
-#     Test the get_stage function returns a valid USD stage.
-#     """
-#     # Create temporary directory and file for testing
-#     with tempfile.TemporaryDirectory() as temp_dir:
-#         test_stage_name = "test_stage.usda"
-#         stage_path = os.path.join(temp_dir, test_stage_name)
+def test_get_stage():
+    """
+    Test the get_stage function returns a valid USD stage.
+    """
+    # Create temporary directory and file for testing
+    with tempfile.TemporaryDirectory() as temp_dir:
+        test_stage_name = "test_stage.usda"
+        stage_path = os.path.join(temp_dir, test_stage_name)
 
-#         # Create a temporary USD stage file
-#         usd_file = Sdf.Layer.CreateNew(stage_path)
-#         assert usd_file != None, "Failed to create temporary USD stage file"
+        # Create a temporary USD stage file
+        usd_file = Sdf.Layer.CreateNew(stage_path)
+        assert usd_file != None, "Failed to create temporary USD stage file"
         
-#         # Test the get_stage function
-#         stage = get_stage(temp_dir, test_stage_name)
-#         assert stage != None, "Failed to get USD stage"
-#         assert isinstance(stage, Usd.Stage), "Returned object is not a USD stage"
-#         assert stage.GetRootLayer().identifier == stage_path, "Stage path does not match expected path"
+        # Test the get_stage function
+        stage = get_stage(temp_dir, test_stage_name)
+        assert stage != None, "Failed to get USD stage"
+        assert isinstance(stage, Usd.Stage), "Returned object is not a USD stage"
+        assert stage.GetRootLayer().identifier == stage_path, "Stage path does not match expected path"
 
 
 def test_create_layer():
