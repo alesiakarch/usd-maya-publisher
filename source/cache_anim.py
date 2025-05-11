fda0d861cc0efb65c9fd949605e39e9dd1e2f936
alesiakarch <karchevskaya.alesya@gmail.com>
2025-05-12 00:20:41 +0100

    nothing works, cant cache anything

--- source/cache_anim.py
+++ source/cache_anim.py
@@ -1,1 +1,18 @@
 # Cache anim button
+# Fetches all avaialble rigs in the usd stage
+# Gives an option to select them
+# Cache selected
+
+import pathlib
+from pxr import Usd, Sdf
+import maya.cmds as cmds
+import source.usd_tool_utils as usd_utils
+
+def cache_anim_button():
+    """
+    Cache the chosen rig into a usd cache
+    """
+    stage = usd_utils.get_stage()
+
+    cache_anim()
+
